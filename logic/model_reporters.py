Fix: use relative stake values during reward calculation + new model reporters + minor code changes

logic/model_reporters.py
@@ -1,6 +1,7 @@
 import statistics
 import collections
 from gekko import GEKKO
+import numpy as np
 
 from logic.helper import MAX_NUM_POOLS
 import logic.helper as hlp
@@ -418,6 +419,43 @@ def get_pool_stakes_by_agent(model):
     return pool_stakes
 
 
+def gini_coefficient(np_array):
+    """Compute Gini coefficient of array of values
+    using the fact that their Gini coefficient is half their relative mean absolute difference,
+    as noted here: https://en.wikipedia.org/wiki/Mean_absolute_difference#Relative_mean_absolute_difference """
+    diffsum = 0 # sum of absolute differences
+    for i, xi in enumerate(np_array[:-1], 1):
+        diffsum += np.sum(np.abs(xi - np_array[i:]))
+    return diffsum / (len(np_array) * sum(np_array)) if sum(np_array) != 0 else -1
+
+
+def get_gini_id_coeff_pool_count(model):
+    """
+    We
+    @param model:
+    @return:
+    """
+    # gather data
+    pools = model.get_pools_list()
+    print(pools)
+    #todo check later if you can abstract this to a function that serves this one, NC and others
+    pools_owned = collections.defaultdict(lambda: 0)
+    for pool in pools:
+        pools_owned[pool.owner] += 1
+    pools_per_agent = np.fromiter(pools_owned.values(), dtype=int)
+    return gini_coefficient(pools_per_agent)
+
+
+def get_gini_id_coeff_stake(model):
+    pools = model.get_pools_list()
+    # todo check later if you can abstract this to a function that serves this one, NC and others
+    stake_controlled = collections.defaultdict(lambda: 0)
+    for pool in pools:
+        stake_controlled[pool.owner] += pool.stake
+    stake_per_agent = np.fromiter(stake_controlled.values(), dtype=float)
+    return gini_coefficient(stake_per_agent)
+
+
 # note that any new model reporters should be added to the end, to maintain the colour allocation
 all_model_reporters = {
     "Pool count": get_final_number_of_pools,
@@ -440,5 +478,7 @@ all_model_reporters = {
     "Median cost rank": get_median_cost_rnk,
     "Opt min aggr pledge": get_optimal_min_aggregate_pledge,
     "Number of pool splitters": get_pool_splitter_count,
-    "Cost efficient stakeholders": get_cost_efficient_count
+    "Cost efficient stakeholders": get_cost_efficient_count,
+    "Gini-id": get_gini_id_coeff_pool_count,
+    "Gini-id stake": get_gini_id_coeff_stake
 }

tests/test_model_reporters.py
@@ -21,11 +21,14 @@ def test_get_controlled_stake_mean_abs_diff(mocker):
         2: Stakeholder(unique_id=2, model=model, stake=0.04)
     }
 
-    pool1 = Pool(owner=1, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=555, reward_function_option=0)
+    pool1 = Pool(owner=1, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=555,
+                 reward_function_option=0)
     pool1.stake = 0.08
-    pool2 = Pool(owner=2, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=556, reward_function_option=0)
+    pool2 = Pool(owner=2, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=556,
+                 reward_function_option=0)
     pool2.stake = 0.1
-    pool3 = Pool(owner=2, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=557, reward_function_option=0)
+    pool3 = Pool(owner=2, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=557,
+                 reward_function_option=0)
     pool3.stake = 0.05
     pools_list = [pool1, pool2, pool3]
 
@@ -46,11 +49,14 @@ def test_get_controlled_stake_distr_stat_dist(mocker):
         3: Stakeholder(unique_id=3, model=model, stake=0.01)
     }
 
-    pool1 = Pool(owner=1, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=555, reward_function_option=0)
+    pool1 = Pool(owner=1, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=555,
+                 reward_function_option=0)
     pool1.stake = 0.08
-    pool2 = Pool(owner=2, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=556, reward_function_option=0)
+    pool2 = Pool(owner=2, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=556,
+                 reward_function_option=0)
     pool2.stake = 0.1
-    pool3 = Pool(owner=2, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=557, reward_function_option=0)
+    pool3 = Pool(owner=2, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=557,
+                 reward_function_option=0)
     pool3.stake = 0.05
     pools_list = [pool1, pool2, pool3]
 
@@ -66,11 +72,14 @@ def test_get_controlled_stake_distr_stat_dist(mocker):
 def test_get_min_aggregate_pledge(mocker):
     model = logic.sim.Simulation()
 
-    pool1 = Pool(owner=1, cost=0.001, pledge=0.001, margin=0.1, alpha=0.3, beta=0.1, pool_id=555, reward_function_option=0)
+    pool1 = Pool(owner=1, cost=0.001, pledge=0.001, margin=0.1, alpha=0.3, beta=0.1, pool_id=555,
+                 reward_function_option=0)
     pool1.stake = 0.09
-    pool2 = Pool(owner=2, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=556, reward_function_option=0)
+    pool2 = Pool(owner=2, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=556,
+                 reward_function_option=0)
     pool2.stake = 0.1
-    pool3 = Pool(owner=2, cost=0.001, pledge=0.002, margin=0.1, alpha=0.3, beta=0.1, pool_id=557, reward_function_option=0)
+    pool3 = Pool(owner=2, cost=0.001, pledge=0.002, margin=0.1, alpha=0.3, beta=0.1, pool_id=557,
+                 reward_function_option=0)
     pool3.stake = 0.05
     pools_list = [pool1, pool2, pool3]
 
@@ -86,7 +95,8 @@ def test_get_min_aggregate_pledge(mocker):
     stake_per_pool = 0.001
     for i in range(num_pools):
         pools_list.append(
-            Pool(owner=i, cost=0.001, pledge=stake_per_pool, margin=0.1, alpha=0.3, beta=0.1, pool_id=100 + i, reward_function_option=0))
+            Pool(owner=i, cost=0.001, pledge=stake_per_pool, margin=0.1, alpha=0.3, beta=0.1, pool_id=100 + i,
+                 reward_function_option=0))
     mocker.patch('logic.sim.Simulation.get_pools_list', return_value=pools_list)
     min_aggr_pledge = get_min_aggregate_pledge(model)
     assert min_aggr_pledge == num_pools / 2 * stake_per_pool
@@ -107,13 +117,77 @@ def test_get_optimal_min_aggregate_pledge(mocker):
 def test_get_pool_splitter_count(mocker):
     model = logic.sim.Simulation()
 
-    pools_list = [Pool(owner=i, cost=0.001, pledge=0.001, margin=0.1, alpha=0.3, beta=0.1, pool_id=555, reward_function_option=0)
-                  for i in range(1,11)]
-    pools_list.append(Pool(owner=2, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=556, reward_function_option=0))
-    pools_list.append(Pool(owner=2, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=556, reward_function_option=0))
-    pools_list.append(Pool(owner=5, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=556, reward_function_option=0))
+    pools_list = [
+        Pool(owner=i, cost=0.001, pledge=0.001, margin=0.1, alpha=0.3, beta=0.1, pool_id=555, reward_function_option=0, total_stake=1)
+        for i in range(1, 11)]
+    pools_list.append(
+        Pool(owner=2, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=556, reward_function_option=0, total_stake=1))
+    pools_list.append(
+        Pool(owner=2, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=556, reward_function_option=0, total_stake=1))
+    pools_list.append(
+        Pool(owner=5, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=556, reward_function_option=0, total_stake=1))
 
     mocker.patch('logic.sim.Simulation.get_pools_list', return_value=pools_list)
 
     pool_splitter_count = get_pool_splitter_count(model)
     assert pool_splitter_count == 2
+
+
+def test_gini_coefficient():
+    # results retreived from: https://shlegeris.com/gini.html (3 decimal accuracy)
+    x1 = np.array([1, 2, 3, 4, 5])
+    g1 = gini_coefficient(x1)
+    assert round(g1, 3) == 0.267
+
+    x2 = np.array([368, 156, 20, 7, 10, 49, 22, 1])
+    g2 = gini_coefficient(x2)
+    assert round(g2, 3) == 0.678
+
+    x3 = np.array([11, 2, 1])
+    g3 = gini_coefficient(x3)
+    assert round(g3, 3) == 0.476
+
+    x4 = np.array([0.11, 0.06, 0.06])
+    g4 = gini_coefficient(x4)
+    assert round(g4, 3) == 0.145
+
+
+
+def test_get_gini_id_coeff_pool_count():
+    model = logic.sim.Simulation()
+
+    pools = {}
+    pools_1 = [
+        Pool(owner=1, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=i, reward_function_option=0, total_stake=1)
+        for i in range(11)]
+    for pool in pools_1:
+        pools[pool.id] = pool
+    pools[11] = Pool(owner=2, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=11, reward_function_option=0, total_stake=1)
+    pools[12] = Pool(owner=2, cost=0.001, pledge=0.05, margin=0.1, alpha=0.3, beta=0.1, pool_id=12, reward_function_option=0, total_stake=1)
+    pools[13] = Pool(owner=5, cost=0.001, pledge=0.06, margin=0.1, alpha=0.3, beta=0.1, pool_id=13, reward_function_option=0, total_stake=1)
+    model.pools = pools
+
+    g = get_gini_id_coeff_pool_count(model)
+    assert round(g, 3) == 0.476
+
+
+def test_get_gini_id_coeff_stake():
+    model = logic.sim.Simulation()
+
+    pools = {}
+    pools_1 = [
+        Pool(owner=1, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=i,
+             reward_function_option=0, total_stake=1)
+        for i in range(11)]
+    for pool in pools_1:
+        pools[pool.id] = pool
+    pools[11] = Pool(owner=2, cost=0.001, pledge=0.01, margin=0.1, alpha=0.3, beta=0.1, pool_id=11,
+                     reward_function_option=0, total_stake=1)
+    pools[12] = Pool(owner=2, cost=0.001, pledge=0.05, margin=0.1, alpha=0.3, beta=0.1, pool_id=12,
+                     reward_function_option=0, total_stake=1)
+    pools[13] = Pool(owner=5, cost=0.001, pledge=0.06, margin=0.1, alpha=0.3, beta=0.1, pool_id=13,
+                     reward_function_option=0, total_stake=1)
+    model.pools = pools
+
+    g = get_gini_id_coeff_stake(model)
+    assert round(g, 3) == 0.145