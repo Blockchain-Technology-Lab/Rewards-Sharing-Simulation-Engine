c44188f6c51325db3aeaacb97a1d407cdc96049f
LadyChristina <christina_ovezik@outlook.com>
2022-09-13 17:58:42 +0100

    Add command-line option for profiling main

--- main.py
+++ main.py
@@ -43,40 +43,27 @@ def main():
         input_from_file=args.input_from_file
     )
 
+    profiling = args.profile_code
+    if profiling:
+        import cProfile
+        from pstats import Stats
+        pr = cProfile.Profile()
+        pr.enable()
+
     sim.run_model()
-
-    sim_df = sim.datacollector.get_model_vars_dataframe()
-    execution_id = sim.execution_id
-
-    execution_dir = sim.directory
-    figures_dir = execution_dir / "figures"
-    pathlib.Path(figures_dir).mkdir(parents=True, exist_ok=True)
 
     if sim.has_converged():
         print('Reached equilibrium at step: ', sim.equilibrium_steps)
     else:
         print('Maximum iterations reached without convergence to an equilibrium.')
 
-
-def main_with_profiling():
-    import cProfile
-    from pstats import Stats
-
-    pr = cProfile.Profile()
-    pr.enable()
-
-    main()
-
-    pr.disable()
-    stats = Stats(pr)
-    stats.sort_stats('tottime').print_stats(10)
+    if profiling:
+        pr.disable()
+        stats = Stats(pr)
+        print("\n----- Profiling results -----")
+        stats.sort_stats('tottime').print_stats(10)
 
 
 if __name__ == "__main__":
-    main()  # for profiling the code, comment this line and uncomment the one below
-    '''main_with_profiling()
-    cache_funcs = [calculate_potential_profit, calculate_pool_reward, #calculate_cost_per_pool, calculate_myopic_pool_desirability,
-                   calculate_delegator_reward_from_pool, calculate_operator_reward_from_pool, calculate_cost_per_pool_fixed_fraction,
-                   calculate_pool_desirability, calculate_pool_stake_NM_from_rank, determine_pledge_per_pool]
-    for func in cache_funcs:
-        print(func.__name__,': ', func.cache_info())'''
+    main()
+
